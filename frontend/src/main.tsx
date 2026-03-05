3e60e6b272bfa6349beb825fc4e81009e914e683
Samudra Uduwaka <samudrauduwaka@gmail.com>
2026-03-05 19:54:05 +0530

    Disable React Strict Mode in development to prevent double mounting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,8 @@
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom/client'
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // Remove <React.StrictMode> wrapper
+  <App />
+)
